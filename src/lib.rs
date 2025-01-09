c4ce26fd44f04b6144351628e518d7f26d780740
bidhan-a <bidhan619@gmail.com>
2025-01-09 09:14:51 +0545

    initialize

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use solana_sdk;
+
+    #[test]
+    fn keygen() {}
+
+    #[test]
+    fn airdop() {}
+
+    #[test]
+    fn transfer_sol() {}
+}
